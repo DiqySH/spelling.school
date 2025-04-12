d37a805eafce76a37e2e0d4374014adcd7cb61f4
DiqySH <margolang55@gmail.com>
2025-04-12 17:26:09 +0700

    revisi

--- main.js
+++ main.js
@@ -1,27 +1,34 @@
 let switcher = document.getElementById("switcherMode");
 let switcherContainer = document.getElementById("container");
 let light = document.getElementById("light");
-let nav = document.getElementById("navJS");
+let nav = document.getElementById("navbar");
 let hamburger = document.getElementById("hamburger")
 let navMobile = document.getElementById("navForMobile")
+let svg = document.getElementById("svg")
 switcher.addEventListener("click", function() {
     switcherContainer.classList.toggle("transition")
     light.style.opacity = "1";
 })
 
 function myFunction(x) {
-    if (x.matches) { // If media query matches
+    if (x.matches) {
     } else {
     }
   }
   
-  // Create a MediaQueryList object
   var x = window.matchMedia("(max-width: 970px)")
   
-  // Call listener function at run time
   myFunction(x);
   
-  // Attach listener function on state changes
   x.addEventListener("change", function() {
     myFunction(x);
+  });
+
+  window.addEventListener('scroll',(e)=>{
+    const navbar = document.getElementById('navbar');
+    if(window.pageYOffset>0){
+      navbar.classList.add("add-shadow");
+    }else{
+      navbar.classList.remove("add-shadow");
+    }
   });
